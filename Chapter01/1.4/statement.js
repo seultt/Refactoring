9c779153ea23344986098c9083ee1a0f2b618b8f
seulgi.lee <seulgi.lee@byclip.com>
2021-09-05 17:25:13 +0900

    1.4 적립 포인트 계산 코드 추출하기

--- Chapter01/1.4/statement.js
+++ Chapter01/1.4/statement.js
@@ -12,11 +12,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
@@ -30,6 +26,14 @@ function statement(invoice, plays) {
 
   function playFor(aPerformance) {
     return plays[aPerformance.playId];
+  }
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type) {
+      volumeCredits += Math.floor(perf.audience / 5);
+    }
+    return volumeCredits;
   }
   function amountFor(aPerformance) {
     let result = 0;
